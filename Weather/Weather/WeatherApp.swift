8d3bb9dd0ba0899c2269876d64efc4cdebbb1ed9
batunpc <batuhnpc@gmail.com>
2023-10-02 18:20:55 -0400

    learned ui stack view

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Batuhan Ipci on 2023-10-02.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
